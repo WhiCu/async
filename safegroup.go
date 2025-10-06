58ce2aba57bcea6b1eab5d03110ff8bd7debd8f7
WhiCu <maks29032005@gmail.com>
2025-10-06 10:57:49 +0300

    feat(safegroup): add new methods

--- safegroup.go
+++ safegroup.go
@@ -15,8 +15,20 @@ func (wg *SafeGroup) Go(f func()) {
 	})
 }
 
+func (wg *SafeGroup) GoErr(f func() error) {
+	wg.Group.Go(func() error {
+		return try.TryErr(f)
+	})
+}
+
 func (wg *SafeGroup) TryGo(f func()) bool {
 	return wg.Group.TryGo(func() error {
 		return try.Try(f)
+	})
+}
+
+func (wg *SafeGroup) TryGoErr(f func() error) bool {
+	return wg.Group.TryGo(func() error {
+		return try.TryErr(f)
 	})
 }
